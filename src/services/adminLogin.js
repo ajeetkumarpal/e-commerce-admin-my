90a013a3d5ff9ffa5925776782efa67558182497
Ajeet kumar pal <ajeetkumarpal22@gmail.com>
2026-02-16 12:08:31 +0530

    a

--- src/services/adminLogin.js
+++ src/services/adminLogin.js
@@ -3,7 +3,7 @@ import { backendURL } from "../api";
 
 const adminLogin = async (data) => {
   try {
-    console.log(backendURL);
+    console.log(`${backendURL}/api/user/admin`,"before");
 
     const response = await axios.post(`${backendURL}/api/user/admin`, data, {
   
